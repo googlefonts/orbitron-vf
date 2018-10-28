45f766da2c7c3ce5806f9a5013e938066a46b618
Eli Heuer <eliheuer@gmail.com>
2018-10-28 17:59:47 -0400

    Update build script to make output more readable

--- sources/BUILD.py
+++ sources/BUILD.py
@@ -1,38 +1,61 @@
-import os 
+import argparse
+import os
 import subprocess
+from fontTools.ttLib import TTFont
 
 
+# FONTMAKE
 try:
-    print("**** Running Fontmake ******************************\n")
-    subprocess.call(['fontmake', '-g', 'sources/Orbitron.glyphs', '-o', 'variable',])
+    print("\n**** Running Fontmake")
+    print("     [+] Run: fontmake -g sources/Orbitron.glyphs -o variable")
+    subprocess.call("fontmake -g sources/Orbitron.glyphs -o variable > /dev/null 2>&1", shell=True)
+    print("     [+] Done")
 except:
-    print("Error! Try installing Fontmake: https://github.com/googlei18n/fontmake")
+    print("     [!] Error! Try installing Fontmake: https://github.com/googlei18n/fontmake")
 
 
-print("\n**** Moving fonts to fonts directory *******************")
-subprocess.call(['cp', 'variable_ttf/Orbitron-VF.ttf', 'fonts/',])
+# MOVE FONTS
+print("\n**** Moving fonts to fonts directory")
+subprocess.call("cp variable_ttf/Orbitron-VF.ttf fonts/", shell=True)
 print("     [+] Done")
 
 
-print("\n**** Removing build directories  ***********************")
-subprocess.call(['rm', '-rf', 'variable_ttf', 'master_ufo'])
+# CLEANUP
+print("\n**** Removing build directories")
+print("     [+] Run: rm -rf variable_ttf master_ufo")
+subprocess.call("rm -rf variable_ttf master_ufo", shell=True)
 print("     [+] Done")
 
 
+# AUTOHINT
+print("\n**** Run: ttfautohint")
 os.chdir('fonts')
-print("     [+] Done")
 cwd = os.getcwd()
-print("     In Directory:", cwd)
-print("\n**** Run: ttfautohint  *********************************\n")
-subprocess.call(['ttfautohint', '-I', 'Orbitron-VF.ttf', 'Orbitron-VF-Fix.ttf'])
-subprocess.call(['cp', 'Orbitron-VF-Fix.ttf', 'Orbitron-VF.ttf'])
-subprocess.call(['rm', '-rf', 'Orbitron-VF-Fix.ttf'])
+print("     [+] In Directory:", cwd)
+subprocess.call("ttfautohint -I Orbitron-VF.ttf Orbitron-VF-Fix.ttf", shell=True)
+subprocess.call("cp Orbitron-VF-Fix.ttf Orbitron-VF.ttf", shell=True)
+subprocess.call("rm -rf Orbitron-VF-Fix.ttf", shell=True)
 print("     [+] Done")
 
 
+# GFTOOLS
+print("\n**** Run: gftools")
 os.chdir("..")
 cwd = os.getcwd()
-print("     In Directory:", cwd)
-print("\n**** Run: gftools  **************************************")
-# subprocess.call(['gftools', 'fix-dsgi', 'fonts/Staatliches-Regular.ttf', '--autofix'])
+print("     [+] In Directory:", cwd)
+#subprocess.call("gftools fix-dsgi fonts/Foo-VF.ttf --autofix", shell=True)
+
+
+# FONTTOOLS
+print("\n**** Run: edit xAvgCharWidth")
+cwd = os.getcwd()
+print("     [+] In Directory:", cwd)
+font = TTFont('fonts/Orbitron-VF.ttf')
+print("     [+] font =", font)
+print("     [+] Done")
+
+# DRAWBOT
+print("\n**** Run: DrawBot")
+subprocess.call("python3 docs/drawbot-sources/basic-specimen.py > /dev/null 2>&1", shell=True)
+print("     [+] Done")
 
